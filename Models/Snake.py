7112049b10cdca3f56cbc0a18c967784e0c31d07
Juan José Borrero Mejía <juanjoborrero@yahoo.com>
2025-04-01 11:44:42 -0500

    Snake change

--- Models/Snake.py
+++ Models/Snake.py
@@ -1,7 +1,22 @@
 import numpy as np
 
 class Snake:
+    
+    """
+    Class representing the snake in the game.
+    The snake is represented as a list of coordinates on a grid.
+    The snake can grow and change direction.
+    """
+    
     def __init__(self, grid_size=(20, 20)):
+        
+        """
+        Initialize the Snake class.
+        :param grid_size: Size of the grid (height, width).
+        """
+        
+        self.grid_size = grid_size
+        self.body: list[np.ndarray] = []
         self.directions: list[str] = ["up", "down", "left", "right", "none"]
         self.movements: dict[str, np.ndarray] = {
             "up": np.array([0, 1]),
@@ -10,36 +25,28 @@ class Snake:
             "right": np.array([1, 0]),
             "none": np.array([0, 0]),
         }
-        self.grid_size = grid_size
-        self.direction = np.random.choice(self.directions, p=[0.25, 0.25, 0.25, 0.25, 0])
         self.reset()
 
     def reset(self):
+        
+        """
+        Reset the snake to its initial state.
+        The snake is placed at a random position on the grid,
+        and its initial direction is randomly chosen.
+        """
+        
+        self.direction = np.random.choice(
+            self.directions, p=[0.25, 0.25, 0.25, 0.25, 0]
+        )
         head = np.random.randint(low=1, high=self.grid_size[0] - 1, size=2)
         tail = head - self.movements[self.direction]
         self.body = [head, tail]
-
-    def move(self, action):
-        new_direction = self.movements.get(action, self.direction)
-        if not np.array_equal(new_direction, -self.direction):
-            self.direction = new_direction
-        new_head = self.body[0] + self.direction
-        return new_head
-
+    
     def grow(self):
-        self.body.append(self.body[-1])
-
-    def update_body(self, new_head):
-        self.body.insert(0, new_head)
-        self.body.pop()
-
-    def check_collision(self, new_head):
-        if (
-            new_head[0] < 0
-            or new_head[0] >= self.grid_size[0]
-            or new_head[1] < 0
-            or new_head[1] >= self.grid_size[1]
-            or any(np.array_equal(new_head, part) for part in self.body)
-        ):
-            return True
-        return False
+        
+        """
+        Grow the snake by adding a new segment to its body.
+        The new segment is placed as the tail
+        """
+        
+        self.body.append(self.body[-1] - self.movements[self.direction])
